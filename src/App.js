c50e673fa488f23c7720a26844ff944ea032d3fa
kajinFuujin <kteruhino@gmail.com>
2021-08-01 09:25:38 +0900

    first commit to remote github.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Hello Fox World!</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -19,6 +20,7 @@ function App() {
         </a>
       </header>
     </div>
+    
   );
 }
 
